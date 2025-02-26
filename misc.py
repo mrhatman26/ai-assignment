ea71d647f243fd0f2de7a708c2e3f54fb0cfc5ad
mrhatman26 <{ID}+{username}+@user.noreply.github.com>
2025-02-26 12:11:59 +0000

    Removed unused functions from misc.py

--- misc.py
+++ misc.py
@@ -23,89 +23,9 @@ def str_to_int_test(text):
     else:
         return False
 
-def get_valid_number(message): #Remove
-    no = 0
-    while True:
-        try:
-            no = int(input(message))
-            break
-        except:
-            print("Please enter a valid number!")
-            input("(Press ENTER to continue)")
-            print()
-    return no
-
 def zlen(item):
     #Returns the length of a list, but reduces it by one.
     return len(item) - 1
-
-def print_random_list(list_var): #Remove
-    if type(list_var) == list:
-        if len(list_var) > 0:
-            print(list_var[r.randint(0, len(list_var) - 1)])
-        else:
-            print("ERROR [print_random_list]: Given list is empty.")
-    else:
-        print("ERROR [print_random_list]: Given list is NOT a list!")
-
-def load_file(): #Remove
-    window = t.Tk()
-    window.withdraw()
-    file = t.filedialog.askopenfile(mode="r", filetypes=[("Text File", "*.txt")])
-    window.destroy()
-    if file is None:
-        print("No file selected")
-        return None
-    else:
-        print("File selected: " + str(file.name))
-        return file
-
-def save_file(data): #Remove
-    window = t.Tk()
-    window.withdraw()
-    save_file = t.filedialog.asksaveasfile(filetypes=[("Text File", "*.txt")], defaultextension='*.txt')
-    if save_file is None:
-        print("File save has been cancelled")
-        return False
-    if type(data) == list:
-        for line in data:
-            if type(line) == list:
-                for letter in line:
-                    save_file.write(letter)
-            else:
-                save_file.write(line)
-            save_file.write("\n")
-    else:
-        save_file.write(str(data))
-    print("Data saved to: " + save_file.name)
-    save_file.close()
-    window.destroy()
-    return True
-
-def ask_question(title, message): #Remove
-    window = t.Tk()
-    window.withdraw()
-    answer = messagebox.askyesno(title, message)
-    window.destroy()
-    return answer
-
-def show_error(title, message): #Remove
-    window = t.Tk()
-    window.withdraw()
-    messagebox.showerror(title, message)
-    window.destroy()
-
-def show_message(title, message): #Remove
-    window = t.Tk()
-    window.withdraw()
-    messagebox.showinfo(title, message)
-    window.destroy()
-
-def show_warning(title, message): #Remove
-    window = t.Tk()
-    window.withdraw()
-    messagebox.showwarning(title, message)
-    window.destroy()
 
 def get_os_clear():
     #Detects the OS being used and returns the clear screen command for that OS.
