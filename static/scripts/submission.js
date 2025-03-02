704743c60051718e16ba6d40ab9370907421bfc3
mrhatman26 <{ID}+{username}+@user.noreply.github.com>
2025-03-02 19:10:43 +0000

    Added submission function (only validates data for now; it does not submit to the server)

--- static/scripts/submission.js
+++ static/scripts/submission.js
@@ -1,5 +1,6 @@
 console.log("submission.js loaded");
 let form = document.getElementById("submission_form");
+let submit_button = form[7];
 //Genre variables
 let genre_select = form[0];
 let genre_add = form[1];
@@ -10,6 +11,12 @@ let genres_original_vals = [];
 for (var i = 0; i < genre_select.length; i++){
     genres_original_vals.push(genre_select[i].value);
 }
+
+//Other variables
+let age_select = form[3];
+let month_select = form[4];
+let runtime_select = form[5];
+let model_select = form[6];
 
 //Genres Functions
 function addGenre(event){
@@ -45,11 +52,7 @@ function clearGenres(event){
     }
 }
 
-//Age Functions
-
-//Date Functions
-
-//Runtime functions
+//Misc functions
 function toInt(value){
     if (!isNaN(value)){
         return parseInt(value);
@@ -59,7 +62,31 @@ function toInt(value){
     }
 }
 
+//Submit function
+function submit_data(event){
+    //event.preventDefault();
+    if (selected_genres_array.length < 1){
+        var error_message = document.createElement("p");
+        error_message.innerHTML = "Please select atleast one genre.";
+        error_message.style.color = "red";
+        document.getElementById("page_header").appendChild(error_message).scrollTo();
+    }
+    //if (runtime_select.value === "")
+    var submission_data = {
+        "submission_genre": selected_genres_array,
+        "submission_age": age_select.value,
+        "submission_month": month_select.value,
+        "submission_runtime": runtime_select.value,
+        "submission_model": model_select.value
+    };
+    console.log(submission_data["submission_genre"]);
+    console.log(submission_data["submission_age"]);
+    console.log(submission_data["submission_month"]);
+    console.log(submission_data["submission_runtime"]);
+    console.log(submission_data["submission_model"]);
+}
+
 //Genres Event Listeners
 genre_add.addEventListener("click", addGenre);
 genre_clear.addEventListener("click", clearGenres);
-console.log(toInt("50"));
+submit_button.addEventListener("click", submit_data)
